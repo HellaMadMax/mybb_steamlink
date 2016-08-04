a35e6c088ef0b54b2a0b238feb8216aae3bc9a47
HellaMadMax <TheHellaMadMax@gmail.com>
2016-08-05 00:47:49 +1000

    Version 0.2.0 (WIP)
    Moved messages to language files
    Changed code to use language system
    Updated OpenID library
    Added settings:
    - Linking when registering is now optional
    - Forcing existing users to link is now optional
    - Allowing users to login with steam is now optional
    - Options for showing on Profiles and Posts
    - Improved error handling
    Code cleanup

---
+++ inc/languages/english/steamlink.lang.php
@@ -0,0 +1,18 @@
+<?php
+$l["steam_account"] = "Steam Account:";
+$l["users_steam_info"] = "{1}'s Steam Info";
+$l["steamid_64"] = "SteamID 64:";
+$l["steamid_32"] = "SteamID 32:";
+$l["profile_link"] = "Profile Link:";
+$l["not_linked"] = "Not Linked";
+
+$l["steamlink_authenticated"] = 'You have successfully been authenticated by <a href="https://www.steampowered.com" target="_blank">Steam</a>.<br />You will now be taken back to the forum index.';
+$l["steamlink_authenticated_register"] = 'You have successfully been authenticated by <a href="https://www.steampowered.com" target="_blank">Steam</a>.<br />You will now be taken back to registering page.';
+
+$l["steamlink_regalreadylinked"] = "You cannot register using that Steam account as it is already linked to {1}.";
+$l["steamlink_exalreadylinked"] = "You cannot link that Steam account as it is already linked to {1}.";
+
+$l["steamlink_loggedin"] = 'You have successfully been logged in.<br />You will now be taken back to the forum index.';
+$l["steamlink_openidfail"] = "You could not be authenticated with OpenID.";
+$l["steamlink_openidredirect"] = "You are being redirected to Steam to confirm your identity.";
+$l["steamlink_linkrequired"] = "You did not link your steam account. Please link your steam account before continuing.";
